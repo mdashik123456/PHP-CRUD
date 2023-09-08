01c7cac1d87291244c8cd10753b66320e0b881fd
Mohammad Ashiqur Rahman <39029566+mdashik123456@users.noreply.github.com>
2023-09-09 01:48:42 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "localhost"; //give your server name
+$username = "root"; //give your server username
+$password = ""; //give your server pass if available
+
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
@@ -10,12 +11,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
+// Create database automatically
 $sql = "CREATE DATABASE IF NOT EXISTS php_crud;";
 if ($conn->query($sql) === TRUE) {
     // echo "Database created successfully";
     $conn->close();
 
+    //create table automatically
     $sql = "CREATE TABLE IF NOT EXISTS crud (
             id varchar(25) NOT NULL,
             name varchar(255) NOT NULL,
